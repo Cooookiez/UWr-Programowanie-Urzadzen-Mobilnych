f73f8f005c8cacddd70ee5930aa096de83d202e4
Krzysiek (Chris) <kukizk@gmail.com>
2020-11-11 14:30:15 +0100

    Zad_02_06 – Init

---
+++ Listy/Zad_02_06_PhysicsQuizExtended/app/src/main/java/com/example/zad_02_06_physicsquizextended/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zad_02_06_physicsquizextended
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
