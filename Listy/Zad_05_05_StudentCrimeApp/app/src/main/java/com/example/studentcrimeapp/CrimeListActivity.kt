60548e114674c895ef543d18cdd968d215bb4c10
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-17 22:09:10 +0100

    Delete files

--- Listy/Zad_05_05_StudentCrimeApp/app/src/main/java/com/example/studentcrimeapp/CrimeListActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.studentcrimeapp
-
-import androidx.fragment.app.Fragment
-
-class CrimeListActivity : ListFragmentActivity() {
-    override fun createFragment(): Fragment {
-        return CrimeListFragment()
-    }
-}
