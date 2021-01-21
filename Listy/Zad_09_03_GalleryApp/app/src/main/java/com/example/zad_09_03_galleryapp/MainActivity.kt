5281244993b3d1169118e967e7b03d4387f8f0ad
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-21 10:10:56 +0100

    Lab_09_Zad_03 – Camera & Galery

--- Listy/Zad_09_03_GalleryApp/app/src/main/java/com/example/zad_09_03_galleryapp/MainActivity.kt
+++ Listy/Zad_09_03_GalleryApp/app/src/main/java/com/example/zad_09_03_galleryapp/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.example.zad_09_03_galleryapp
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import com.example.zad_09_03_galleryapp.databinding.ActivityMainBinding
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
+import java.io.IOException
 import java.security.acl.AclNotFoundException
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val CAMERA_PERMISSION_CODE = 0
+        private val CAMERA_INTENT = 1
+        private val GALERY_INTENT = 3
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -35,15 +51,12 @@ class MainActivity : AppCompatActivity() {
                 .setItems(dialogItems) { _, which ->
                     when(which) {
                         0 -> {
-                            Log.d("zaq1", "0 -> {")
+                            Log.d("zaq1", "choosePhoto")
                             choosePhoto()
                         }
                         1 -> {
-                            Toast.makeText(
-                                    this,
-                                    "Capture picture selected",
-                                    Toast.LENGTH_SHORT
-                            ).show()
+                            Log.d("zaq1", "chooseCamera")
+                            chooseCamera()
                         }
                     }
                 }
@@ -51,21 +64,72 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun choosePhoto() {
-        Dexter.withContext(this).withPermissions(
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ).withListener(object: MultiplePermissionsListener{
-
-            override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport?) {
-                Log.d("zaq1", "onPermissionsChecked")
-                if (multiplePermissionsReport != null && multiplePermissionsReport.areAllPermissionsGranted()) {
-                   Log.d("zaq1", "dd")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            when(requestCode) {
+                CAMERA_INTENT -> {
+                    val thumbnail: Bitmap = data.extras?.get("data") as Bitmap
+                    binding.imageViewPicture.setImageBitmap(thumbnail)
+                }
+                GALERY_INTENT -> {
+                    val contentUri: Uri? = data.data
+                    try {
+                        if (Build.VERSION.SDK_INT >= 29)  { // for newer versions
+                            val selectedPicture: ImageDecoder.Source =
+                                    ImageDecoder.createSource(this.contentResolver, contentUri!!)
+                            binding.imageViewPicture.setImageBitmap(
+                                    ImageDecoder.decodeBitmap(selectedPicture)
+                            )
+                        } else { // for older versions
+                            val selectedPicture: Bitmap =
+                                MediaStore.Images.Media.getBitmap(contentResolver, contentUri)
+                            binding.imageViewPicture.setImageBitmap(selectedPicture)
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
                 }
             }
+        }
+    }
 
-            override fun onPermissionRationaleShouldBeShown(list: MutableList<PermissionRequest>?, p1: PermissionToken?) {
-                Log.d("zaq1", "pre showRationalDialog")
+    private fun chooseCamera() {
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.CAMERA)
+            .withListener(object: PermissionListener {
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(intent, CAMERA_INTENT)
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
+                    showRationalDialog()
+                }
+            }).onSameThread().check()
+    }
+
+    private fun choosePhoto() {
+        Dexter.withContext(this).withPermission(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+        ).withListener(object: PermissionListener {
+            override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                val intent = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+                startActivityForResult(intent, GALERY_INTENT)
+            }
+
+            override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onPermissionRationaleShouldBeShown(p0: PermissionRequest?, p1: PermissionToken?) {
                 showRationalDialog()
             }
         }).check()
