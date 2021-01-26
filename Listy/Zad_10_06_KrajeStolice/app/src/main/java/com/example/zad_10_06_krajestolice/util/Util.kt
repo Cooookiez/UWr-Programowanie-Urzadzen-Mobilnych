0bd709637b378ab398fc32e4439ed40335256698
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-26 12:51:21 +0100

    Lab_10_Zad_06 – Zmiana flag

--- Listy/Zad_10_06_KrajeStolice/app/src/main/java/com/example/zad_10_06_krajestolice/util/Util.kt
+++ Listy/Zad_10_06_KrajeStolice/app/src/main/java/com/example/zad_10_06_krajestolice/util/Util.kt
@@ -2,11 +2,8 @@ package com.example.zad_10_06_krajestolice.util
 
 import android.content.Context
 import android.net.Uri
-import android.util.Log
 import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.example.zad_10_06_krajestolice.R
 import com.github.twocoffeesoneteam.glidetovectoryou.GlideToVectorYou
 
@@ -19,20 +16,10 @@ fun getProgressDrawable(context: Context): CircularProgressDrawable {
 }
 
 fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
-
-//    val option = RequestOptions()
-//        .placeholder(progressDrawable)
-//        .error(R.drawable.physicyst)
-
-//    Glide.with(context)
-//        .setDefaultRequestOptions(option)
-//        .load(uri)
-//        .into(this)
-
     GlideToVectorYou
         .init()
         .with(context)
         // setPlaceHolder(loading, error)
-        .setPlaceHolder(R.drawable.physicyst, R.drawable.physicyst)
+        .setPlaceHolder(R.drawable.rainbow_flag, R.drawable.question_mark_flag)
         .load(Uri.parse(uri), this)
 }
