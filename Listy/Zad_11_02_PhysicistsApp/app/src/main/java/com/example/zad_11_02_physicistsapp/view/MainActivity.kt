de2491ca6e30da024f22e0d8ecb28df3e155f9ba
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-26 03:36:49 +0100

    Lab_11_Zad_02 – Navigation (+ Init)

---
+++ Listy/Zad_11_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zad_11_02_physicistsapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zad_11_02_physicistsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
