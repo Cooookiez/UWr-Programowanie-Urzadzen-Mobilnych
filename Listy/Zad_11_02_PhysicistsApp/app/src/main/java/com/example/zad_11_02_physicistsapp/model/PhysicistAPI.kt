ecd1e819cceb5ca07b6aaaf4a998fb56172b6e8f
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-26 05:37:28 +0100

    Lab_11_Zad_02 – json z fizykami

--- Listy/Zad_11_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/model/PhysicistAPI.kt
+++ Listy/Zad_11_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/model/PhysicistAPI.kt
@@ -4,6 +4,6 @@ import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 
 interface PhysicistAPI {
-    @GET("Cooookiez/UWr-Programowanie-Urzadzen-Mobilnych/7163fa2891d5aa3dfdd0ec9d2cb62fb1c38a946b/Listy/Zad_11_02_PhysicistsApp/physicistv7.json")
+    @GET("Cooookiez/UWr-Programowanie-Urzadzen-Mobilnych/master/Listy/Zad_11_02_PhysicistsApp/physicistv7.json")
     fun getPhysicists(): Single<List<Physicist>>
 }
