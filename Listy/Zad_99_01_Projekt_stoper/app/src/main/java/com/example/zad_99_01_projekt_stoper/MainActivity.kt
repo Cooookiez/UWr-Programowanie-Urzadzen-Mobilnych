4f332e1d65ced4ee26fe06b96152d2971d3aba3c
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-26 15:04:47 +0100

    Projekt – init

---
+++ Listy/Zad_99_01_Projekt_stoper/app/src/main/java/com/example/zad_99_01_projekt_stoper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.zad_99_01_projekt_stoper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
