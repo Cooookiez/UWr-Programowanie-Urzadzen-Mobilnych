567b9bb520e39b65162c3fd24d1eb686747374cd
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-27 03:07:41 +0100

    Lab_12_Zad_02 – Palette

---
+++ Listy/Zad_12_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/model/PhysicistPalette.kt
@@ -0,0 +1,7 @@
+package com.example.zad_11_02_physicistsapp.model
+
+class PhysicistPalette(
+        var backgroundColor: Int,
+        var textColor: Int,
+        var titleColor: Int
+    )
--- Listy/Zad_12_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/view/detail/DetailFragment.kt
+++ Listy/Zad_12_02_PhysicistsApp/app/src/main/java/com/example/zad_11_02_physicistsapp/view/detail/DetailFragment.kt
@@ -1,14 +1,22 @@
 package com.example.zad_11_02_physicistsapp.view.detail
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.palette.graphics.Palette
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.zad_11_02_physicistsapp.R
 import com.example.zad_11_02_physicistsapp.databinding.FragmentDetailBinding
 import com.example.zad_11_02_physicistsapp.databinding.FragmentListBinding
+import com.example.zad_11_02_physicistsapp.model.PhysicistPalette
 import com.example.zad_11_02_physicistsapp.util.getProgressDrawable
 import com.example.zad_11_02_physicistsapp.util.loadImage
 import com.example.zad_11_02_physicistsapp.viewmodel.DetailViewModel
@@ -44,18 +52,38 @@ class DetailFragment : Fragment() {
         viewModel.physicist.observe(viewLifecycleOwner, { physicist ->
             physicist?.let {
                 binding.physicist = physicist
-//                binding.dvName.text = physicist.name
-//                binding.dvLife.text = physicist.life
-//                binding.dvNationality.text = physicist.nationality
-//                binding.dvMotivation.text = physicist.motivation
-//                context?.let {
-//                    binding.dvPicture.loadImage(
-//                        physicist.imageUrl,
-//                        getProgressDrawable(it)
-//                    )
-//                }
+                it.imageUrl.let { url ->
+                    setupBackgroundColor(url)
+                }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String?) {
+        Glide
+            .with(this)
+            .asBitmap()
+            .load(url)
+            .into(object: CustomTarget<Bitmap>() {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val swatch = palette?.lightVibrantSwatch
+                            val backgroundColor: Int = swatch?.rgb?:0 // <- default zero
+                            val textColor: Int = swatch?.bodyTextColor?:Color.BLACK
+                            val titleColor: Int = swatch?.titleTextColor?:Color.BLACK
+                            val physicistPalette = PhysicistPalette(
+                                backgroundColor,
+                                textColor,
+                                titleColor
+                            )
+                            binding.palette = physicistPalette
+                        }
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) { }
+
+            })
     }
 
     override fun onDestroy() {
