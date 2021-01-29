f964defc8f459bed4ba9ced7481f0135f343a22d
Krzysiek (Chris) <kukizk@gmail.com>
2021-01-29 12:47:16 +0100

    Projekt – od nowa bo coś się popsuło przy init

---
+++ Listy/Zad_99_01_Projekt_StopWatch/app/src/main/java/pl/cookiez/zad_99_01_projekt_stopwatch/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.cookiez.zad_99_01_projekt_stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
